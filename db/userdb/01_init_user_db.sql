68a7c8960a4eaf3f7439a57f9846560c783169a2
daophanquochoai <hoai23828@gmail.com>
2025-12-25 17:45:34 +0700

    #add : cấu hình gateway

--- db/userdb/01_init_user_db.sql
+++ db/userdb/01_init_user_db.sql
@@ -30,3 +30,7 @@ DROP TRIGGER IF EXISTS trg_users_updated_at ON users;
 CREATE TRIGGER trg_users_updated_at
     BEFORE UPDATE ON users
     FOR EACH ROW EXECUTE FUNCTION set_updated_at();
+
+INSERT INTO public.users (id,email,password_hash,"role",created_at,updated_at,deleted_at,name) VALUES
+                                                                                                   ('2294c66d-79df-4d99-974e-6cac94bc8796'::uuid,'quochoai@example.com','$2a$10$8ynQ6uXQLdsO2nesbV0gueFrrSLvSDX93V9aCUb2TyzHFxaJ1IUuq','admin','2025-12-22 23:06:41.627707+07','2025-12-23 17:45:00.458004+07',NULL,'QuocHoai'),
+                                                                                                   ('799f4f24-0b3e-4e7f-88e2-4ebe5b929e69'::uuid,'hongnghi@example.com','$2a$10$Wcr0qb.UU95hQNlvkROo2O3J98pLPqNtyPkdQkEkmXYoErN2C5Noq','user','2025-12-23 22:30:45.530594+07','2025-12-23 22:34:09.171409+07',NULL,'HongNghi');
